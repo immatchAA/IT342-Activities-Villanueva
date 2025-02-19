d12302dfa038822549d2db8373f337a349e80362
LAB13_USER ON WS30 <L13X13W30@citulab.net>
2025-02-19 12:35:00 +0800

    OAuth2 Login Initial commit

---
+++ g2-villanueva-richelle/auth2Login/src/main/java/com/villanueva/auth2Login/Auth2LoginApplication.java
@@ -0,0 +1,13 @@
+package com.villanueva.auth2Login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Auth2LoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Auth2LoginApplication.class, args);
+	}
+
+}
---
+++ g2-villanueva-richelle/auth2Login/src/test/java/com/villanueva/auth2Login/Auth2LoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.villanueva.auth2Login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Auth2LoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
