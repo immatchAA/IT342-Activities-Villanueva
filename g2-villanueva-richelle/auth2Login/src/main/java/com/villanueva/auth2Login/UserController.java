340d214d304ba40d4eef6ab75c643e7137f1e7b5
LAB13_USER ON WS30 <L13X13W30@citulab.net>
2025-02-19 14:38:21 +0800

    OAuth2 Login Google and Github

--- g2-villanueva-richelle/auth2Login/src/main/java/com/villanueva/auth2Login/UserController.java
+++ g2-villanueva-richelle/auth2Login/src/main/java/com/villanueva/auth2Login/UserController.java
@@ -1,2 +1,27 @@
-package com.villanueva.auth2Login;public class UserController {
+package com.villanueva.auth2Login;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class UserController {
+
+    @GetMapping
+    public String index() {
+        return "<h1>Welcome, This is a Landing Page </h1>";
+    }
+    @GetMapping("/user-info")
+    public Map<String, Object> getUserProfile(@AuthenticationPrincipal OAuth2User oAuth2user) {
+        return oAuth2user.getAttributes();
+    }
+
+    @GetMapping("secured")
+    public String securedEndpoint(){
+        return "<h1>This is a secured endpoint.</h1>";
+    }
+
 }
--- g2-villanueva-richelle/auth2Login/src/main/java/com/villanueva/auth2Login/config/SecurityConfig.java
+++ g2-villanueva-richelle/auth2Login/src/main/java/com/villanueva/auth2Login/config/SecurityConfig.java
@@ -1,2 +1,25 @@
-package com.villanueva.auth2Login.config;public class SecurityConfig {
+package com.villanueva.auth2Login.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain defaultSecurityChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(oauth -> oauth.anyRequest().authenticated())
+                .oauth2Login(oauth2Login -> oauth2Login.defaultSuccessUrl("/user-info", true))
+                .formLogin(formLogin -> formLogin.defaultSuccessUrl("/secured", true))
+                .logout(logout -> logout.logoutSuccessUrl("/"))
+                .csrf(AbstractHttpConfigurer::disable)
+                .build();
+    }
+
+
 }
